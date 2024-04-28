1634487a2859e691ab50a02fe53e07424e3061e4
SAMYESSAM30 <samyessam2000@gmail.com>
2024-04-28 15:18:40 +0300

    adit and delete math method

--- src/Components/WeatherApp/WeatherApp.jsx
+++ src/Components/WeatherApp/WeatherApp.jsx
@@ -27,8 +27,8 @@ const WeatherApp = () => {
     const temrature = document.getElementsByClassName("weather-temp");
     const location = document.getElementsByClassName("weather-location");
     humidity[0].innerHTML = data.main.humidity + "%";
-    wind[0].innerHTML = Math.floor(data.wind.speed) + " km/h";
-    temrature[0].innerHTML = Math.floor(data.main.temp) + "°C";
+    wind[0].innerHTML = data.wind.speed + " km/h";
+    temrature[0].innerHTML = data.main.temp + "°C";
     location[0].innerHTML = data.name;
 
     if (data.weather[0].icon === "01d" || data.weather[0].icon === "01n") {
